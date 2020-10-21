4e3392bd55de1c31b7daaf10a055b4072d6c5b36
shihualu <shihualu@umail.ucsb.edu>
2020-10-21 15:12:26 -0700

    Another toy example

---
+++ examples/my_example/hashtable.c
@@ -0,0 +1,20 @@
+#include "klee/klee.h"
+#define N 5
+
+int search(int hashtable[], unsigned int val){
+    int hash = val % N;
+    int temp = hash;
+    while (hashtable[temp] >= 0 && hashtable[temp] % N == hash){
+        if (hashtable[temp] == val) return temp;
+        temp = (temp + 1) % N;
+        if (temp == hash) break;
+    }
+    return -1;
+}
+int main(){
+    int hashtable[N];
+    klee_make_symbolic(hashtable, sizeof hashtable, "hashtable");
+    unsigned int val;
+    klee_make_symbolic(&val, sizeof val, "val");
+    search(hashtable, val);
+}
